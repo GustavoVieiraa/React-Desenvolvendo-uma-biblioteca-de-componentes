88905ab712061fde2c1bc32a6a42a8c025e09c13
Gustavo Vieira <gustavo.vieira@ituran.com.br>
2025-02-25 21:34:39 -0300

    feat: Implementado o Componente Card, e adicionado no StoryBook.

--- example/index.tsx
+++ example/index.tsx
@@ -2,11 +2,13 @@ import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { GdBotao } from '../.';
+import { Card } from '../src/Components/Card/';
 
 const App = () => {
   return (
     <div>
       <GdBotao />
+      <Card children />
     </div>
   );
 };
